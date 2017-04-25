f273a7b50f3c586b964d4023016cb7db43ff24fc
Jessica Gilligan <jessicagilligan@Jessicas-MacBook-Pro.local>
2017-04-24 21:00:36 -0400

    initial work started

---
+++ app.js
@@ -0,0 +1,59 @@
+//set up dependencies
+// =============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require ('fs')
+var friends = require('./friends.js');
+//set up Express App
+// =============================================================
+var app = express();
+var PORT = 8080;
+// Sets up the Express app to handle data parsing
+// =============================================================
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/json"
+}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname)));
+
+//set up routes
+// =============================================================
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/survey", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+app.get("/api/friends", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+app.post("/add", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  var newFriend = req.body;
+  console.log(newFriend);
+  // We then add the json the user sent to the character array
+  characters.push(newFriend);
+  // We then display the JSON to the users
+  console.log(friends.friends);
+  res.json(newFriend);
+});
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+  console.log(friends);
+});
---
+++ friends.js
@@ -0,0 +1,7 @@
+var friends = [{
+    name: "Jessie",
+    photo: "picture.jpg",
+    answers: [1,2,3,4,5,6,7,8,9,10]
+}];
+
+exports.friends = friends;
